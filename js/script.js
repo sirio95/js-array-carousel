a6647a124e9a227ac5402dbf7658b8b80071f88e
Sirio <sirio.iasevoli@gmail.com>
2022-11-13 11:24:54 +0100

    next e prev funzionanti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const items_inizio= [`<div class="item active"><img src="img/01.webp"></div>`, `<div class="item"><img src="img/02.webp"></div>`, `<div class="item"><img src="img/03.webp"></div>`, `<div class="item"><img src="img/04.webp"></div>`, `<div class="item"><img src="img/05.webp"></div>`];
 const dots_inizio=[`<span class="dot one active"></span>`, `<span class="dot two"></span>`, `<span class="dot three"></span>`, `<span class="dot four"></span>`, `<span class="dot five"></span>`]
 
-let next_inizio= `<button class="icon next" id="next_button"><i class="fa-solid fa-right-long"></i></button>`;
-let prev_inizio= `<button class="icon prev active" id="prev_button"><i class="fa-solid fa-left-long"></i></button>`; 
+let next_inizio= `<button class="icon next active" id="next_button"><i class="fa-solid fa-right-long"></i></button>`;
+let prev_inizio= `<button class="icon prev" id="prev_button"><i class="fa-solid fa-left-long"></i></button>`; 
 
 
 const container=document.querySelector(".container");
@@ -38,15 +38,33 @@ inizio.addEventListener('click',
                     active_item++;
                     items[active_item].classList.add('active');
                     dots[active_item].classList.add('active');
-                    prev.classList.remove('active');
-                }else if(active_item === 3){
-                    next.classList.add('active');
+                    prev.classList.add('active');
+                }else if(active_item = 4){
+                    next.classList.remove('active');
                 }
 
             }
-)
+        )
+
+        prev.addEventListener("click",
+            function(){
+                if(active_item != 0 && active_item <= 4){
+                    items[active_item].classList.remove('active');
+                    dots[active_item].classList.remove('active');
+                    active_item--;
+                    items[active_item].classList.add('active');
+                    dots[active_item].classList.add('active');
+                    prev.classList.add('active');
+                }else if(active_item = 0){
+                    prev.classList.remove('active');
+                }
+            }
+        )
+
     }
 )
+    
+
 
 
 
